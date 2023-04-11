c7b50ef32076008b7967ab18bc16ab8d2cba3683
Narine <115227487+narinearut@users.noreply.github.com>
2023-04-11 13:22:21 +0100

    HW11

--- src/axiosFetch.js
+++ src/axiosFetch.js
@@ -3,6 +3,8 @@ import axios from 'axios'
 
 export class FetchForMyHW {
     BASE_URL = 'https://pixabay.com/api/'
+export class FetchJs {
+    URL_KEY = 'https://pixabay.com/api/'
     API_KEY = 'key=34770322-1d785185ad6fb3686a5689e8d'
 
     querry = null
@@ -11,6 +13,7 @@ export class FetchForMyHW {
 
     async axiosReturn () {
        return axios.get(`${this.BASE_URL}?${this.API_KEY}&q=${this.querry}&image_type=photo&safesearch=true&orientation=horizontal&page=${this.page}&per_page=${this.per_page}`).then(data => {return data})
+       return axios.get(`${this.URL_KEY}?${this.API_KEY}&q=${this.querry}&image_type=photo&safesearch=true&orientation=horizontal&page=${this.page}&per_page=${this.per_page}`).then(data => {return data})
     }
     
 }
--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import axios from 'axios';
+
 import Notiflix from 'notiflix';
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 import { FetchForMyHW } from './axiosFetch';
+import { FetchJs } from './axiosFetch';
 import { debounce } from 'debounce';
 
 const fetchFor = new FetchForMyHW();
@@ -10,14 +12,24 @@ const formEL = document.querySelector('#search-form');
 const btnSubmitEl = document.querySelector('button');
 const divEl = document.querySelector('.gallery');
 const btnLoadEl = document.querySelector('.load-more');
+const fetchJs = new FetchJs();
+const form = document.querySelector('#search-form');
+const btnSubmit = document.querySelector('button');
+const section = document.querySelector('.gallery');
+const btnLoadmore = document.querySelector('.load-more');
 const TIME = 400;
 
 btnLoadEl.classList.add('is-hidden');
 btnSubmitEl.setAttribute('disabled', true);
+btnLoadmore.classList.add('is-hidden');
+btnSubmit.setAttribute('disabled', true);
 
 formEL.addEventListener('submit', onBtnSubmit);
 btnLoadEl.addEventListener('click', debounce(onBtnLoadClick, TIME));
 formEL.addEventListener('input', onBtnInput);
+form.addEventListener('submit', onBtnSubmit);
+btnLoadmore.addEventListener('click', debounce(onBtnLoadClick, TIME));
+form.addEventListener('input', onBtnInput);
 // window.addEventListener('scroll', infinityScroll);
 
 function onBtnInput(e) {
@@ -25,6 +37,10 @@ function onBtnInput(e) {
   fetchFor.querry
     ? btnSubmitEl.removeAttribute('disabled')
     : btnSubmitEl.setAttribute('disabled', true);
+    fetchJs.querry = e.target.value.trim();
+    fetchJs.querry
+    ? btnSubmit.removeAttribute('disabled')
+    : btnSubmit.setAttribute('disabled', true);
 }
 
 async function onBtnSubmit(e) {
@@ -33,10 +49,16 @@ async function onBtnSubmit(e) {
   divEl.innerHTML = '';
   fetchFor.querry = e.target.elements.searchQuery.value.trim();
   btnLoadEl.classList.add('is-hidden');
+  fetchJs.page = 1;
+  section.innerHTML = '';
+  fetchJs.querry = e.target.elements.searchQuery.value.trim();
+  btnLoadmore.classList.add('is-hidden');
   try {
     const { data } = await fetchFor.axiosReturn();
+    const { data } = await fetchJs.axiosReturn();
     makeMurkup(data.hits);
     noMoreResult(data.totalHits);
+    resultUndefined(data.totalHits);
 
     if (!data.hits.length) {
       Notiflix.Notify.failure(
@@ -48,6 +70,8 @@ async function onBtnSubmit(e) {
     if (data.hits.length > 0) {
       Notiflix.Notify.success(`Hooray! We found ${data.totalHits} images.`);
       btnLoadEl.classList.remove('is-hidden');
+      Notiflix.Notify.success(`WOOOW! We found ${data.totalHits} images.`);
+      btnLoadmore.classList.remove('is-hidden');
     }
   } catch (err) {
     console.log(err);
@@ -56,11 +80,14 @@ async function onBtnSubmit(e) {
 
 async function onBtnLoadClick() {
   fetchFor.page += 1;
+    fetchJs.page += 1;
 
   try {
     const { data } = await fetchFor.axiosReturn();
+    const { data } = await fetchJs.axiosReturn();
     makeMurkup(data.hits);
     noMoreResult(data.totalHits);
+    resultUndefined(data.totalHits);
   } catch (err) {
     console.log(err);
   }
@@ -100,6 +127,7 @@ function makeMurkup(data) {
     .join('');
 
   divEl.insertAdjacentHTML('beforeend', murkup);
+    section.insertAdjacentHTML('beforeend', murkup);
 
   var lightbox = new SimpleLightbox('.gallery a', {
     captionSelector: 'img',
@@ -112,10 +140,13 @@ function makeMurkup(data) {
 
 function noMoreResult(totalHits) {
   if (fetchFor.page * fetchFor.per_page > totalHits && totalHits !== 0) {
+function resultUndefined(totalHits) {
+  if (fetchJs.page * fetchJs.per_page > totalHits && totalHits !== 0) {
     Notiflix.Notify.warning(
       "We're sorry, but you've reached the end of search results."
     );
     btnLoadEl.classList.add('is-hidden');
+    btnLoadmore.classList.add('is-hidden');
     return;
   }
 }
